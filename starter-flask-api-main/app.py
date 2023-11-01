9270b1c285ee16ddefd9c6ff34316fb896dd1bd5
abhibgp4 <70908475+abhibgp4@users.noreply.github.com>
2023-11-01 13:35:48 +0530

    Update app.py

--- starter-flask-api-main/app.py
+++ starter-flask-api-main/app.py
@@ -7,5 +7,3 @@ def greeting():
     name = "Abhijeet"  # You can change this to any name you want to display
     return render_template('template.html', name=name)
 
-if __name__ == '__main__':
-    app.run()
