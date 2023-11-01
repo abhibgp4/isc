8501be52ccfa66b99085adcb9c893e63bcaf754e
abhibgp4 <70908475+abhibgp4@users.noreply.github.com>
2023-11-01 13:34:55 +0530

    Update app.py

--- starter-flask-api-main/app.py
+++ starter-flask-api-main/app.py
@@ -5,7 +5,7 @@ app = Flask(__name)
 @app.route('/')
 def greeting():
     name = "Abhijeet"  # You can change this to any name you want to display
-    return render_template('greeting.html', name=name)
+    return render_template('template.html', name=name)
 
 if __name__ == '__main__':
     app.run()
