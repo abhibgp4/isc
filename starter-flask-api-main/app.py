5e66053b85ce21bb2fd7fd9064122d6d1abda3a4
abhibgp4 <70908475+abhibgp4@users.noreply.github.com>
2023-11-01 22:07:01 +0530

    Update app.py

--- starter-flask-api-main/app.py
+++ starter-flask-api-main/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, request, jsonify
 import os
+import boto3
+from botocore.exceptions import NoCredentialsError
 
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app = Flask(__name)
 
-app = Flask(__name__)
+# AWS S3 configuration
+S3_BUCKET_NAME = 'cyclic-rich-ox-handbag-eu-west-2'
+S3_ACCESS_KEY = 'YOUR_ACCESS_KEY'
+S3_SECRET_KEY = 'YOUR_SECRET_KEY'
+
+# Initialize an S3 client
+#s3 = boto3.client('s3', aws_access_key_id=S3_ACCESS_KEY, aws_secret_access_key=S3_SECRET_KEY)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
