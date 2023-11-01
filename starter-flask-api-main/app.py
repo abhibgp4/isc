81c09d8e89e0d92429c6f8fb2a2edb14fa2e3bc3
abhibgp4 <70908475+abhibgp4@users.noreply.github.com>
2023-11-01 22:00:07 +0530

    Update app.py

--- starter-flask-api-main/app.py
+++ starter-flask-api-main/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 import os
 
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
