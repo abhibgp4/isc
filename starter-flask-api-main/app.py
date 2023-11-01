79d796229fb3dfb70709e43ff3604743dbb003d3
abhibgp4 <70908475+abhibgp4@users.noreply.github.com>
2023-11-01 13:43:36 +0530

    Update app.py

--- starter-flask-api-main/app.py
+++ starter-flask-api-main/app.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    name = "Abhijeet"  # You can replace this with any name you want to display
-    return render_template('template.html', name=name)
+    return 'Hello, world! from IIIT BHagalpur '
+
+
+''' 
+
+from flask import Flask, render_template
+
+app = Flask(__name)
+
+@app.route('/')
+def greeting():
+    name = "Abhijeet"  # You can change this to any name you want to display
+    return render_template('greeting.html', name=name)
+
+if __name__ == '__main__':
+    app.run()
+
+
+'''
