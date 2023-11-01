48787687ca281c48cc24eb14ef7921c7227b68f7
abhibgp4 <70908475+abhibgp4@users.noreply.github.com>
2023-11-01 22:22:20 +0530

    Update app.py

--- starter-flask-api-main/app.py
+++ starter-flask-api-main/app.py
@@ -3,7 +3,16 @@ import os
 import boto3
 from botocore.exceptions import NoCredentialsError
 
+# Create a Flask web application
 app = Flask(__name__)
+
+# AWS S3 configuration
+S3_BUCKET_NAME = 'cyclic-rich-ox-handbag-eu-west-2'  # Replace with your S3 bucket name
+S3_ACCESS_KEY = 'YOUR_ACCESS_KEY'  # Replace with your AWS access key
+S3_SECRET_KEY = 'YOUR_SECRET_KEY'  # Replace with your AWS secret key
+
+# Initialize an S3 client using the provided credentials
+#s3 = boto3.client('s3', aws_access_key_id=S3_ACCESS_KEY, aws_secret_access_key=S3_SECRET_KEY)
 
 @app.route('/')
 def hello_world():
