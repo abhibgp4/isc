db2cc6dc89ed6ce150e3232fe24b5e4a1b252229
abhibgp4 <70908475+abhibgp4@users.noreply.github.com>
2023-11-05 21:51:43 +0530

    Update app.py

--- starter-flask-api-main/app.py
+++ starter-flask-api-main/app.py
@@ -1,39 +1,45 @@
 from flask import Flask, render_template, request, jsonify
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-import boto3
-from botocore.exceptions import NoCredentialsError
 import os
+import werkzeug
+import call_test_model_03
+import cv2
+import matplotlib.pyplot as plt
+
+def imgshow():
+    # Read your three images
+    image1 = cv2.imread('templates/IMG/detected_face.jpg')
+    image2 = cv2.imread('templates/IMG/predicted_image.png')  # Replace with the path to your third image
+
+    # Create a figure with three subplots
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+
+    # Display the first image in the first subplot
+    ax1.imshow(cv2.cvtColor(image1, cv2.COLOR_BGR2RGB))  # Convert BGR to RGB for Matplotlib
+    ax1.set_title('Face Detected')
+
+    # Display the second image in the second subplot
+    ax2.imshow(cv2.cvtColor(image2, cv2.COLOR_BGR2RGB))  # Convert BGR to RGB for Matplotlib
+    ax2.set_title('Model Prediction ')
+
+    # Show the figure with all three subplots
+    plt.show()
 
 # Create a Flask web application
 app = Flask(__name__)
 
-'''
-SET AWS_REGION=eu-west-2
-SET AWS_ACCESS_KEY_ID=ASIA2EX77VVO537OKKDI
-'''
-# AWS S3 configuration
-S3_BUCKET_NAME = 'cyclic-rich-ox-handbag-eu-west-2'  # Replace with your S3 bucket name
-S3_ACCESS_KEY = 'ASIA2EX77VVORU6FZ3PI'  # Replace with your AWS access key
-S3_SECRET_KEY = 'BSdH1UkHXyD4Tqj93oLaWCk8HDNO5YWYOE8e3oDB'  # Replace with your AWS secret key
+# Define the folder where uploaded images will be stored
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Initialize an S3 client using the provided credentials
-s3 = boto3.client('s3', aws_access_key_id=S3_ACCESS_KEY, aws_secret_access_key=S3_SECRET_KEY)
+# Ensure the "uploads" folder exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-# Google Drive API configuration
-SCOPES = ['https://www.googleapis.com/auth/drive.file']
-
-SERVICE_ACCOUNT_FILE = 'cred.json'  # Replace with your JSON service account key file
-
-# Create a Google Drive API service
-credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-#drive_service = build('drive', 'v3', credentials=credentials)
-
+# Route to render the index.html template
 @app.route('/')
-def hello_world1():
+def hello_world():
     return render_template('index.html')
 
-# Define a route to handle the image upload
+# Route to handle image uploads
 @app.route('/upload', methods=['POST'])
 def upload():
     # Check if the 'image' file is included in the POST request
@@ -45,20 +51,21 @@ def upload():
     # Check if the filename is empty (no file selected)
     if image.filename == '':
         return jsonify({'success': False, 'message': 'No selected file'})
+    
+    # Extract the original file extension from the filename
+    file_extension = werkzeug.utils.secure_filename(image.filename).rsplit('.', 1)[1]
 
-    if image:
-        try:
-            # Generate a unique file name or use the original file name
-            filename = 'uploaded_image.jpg'  # You can modify this as needed
+    # Save the uploaded image with the same filename and extension
+    image.save(os.path.join(app.config['UPLOAD_FOLDER'], f'test.{file_extension}'))
 
-            # Upload the image to the S3 bucket
-            s3.upload_fileobj(image, S3_BUCKET_NAME, filename)
+    # Call the process_image function from the emotion model module
+    result = call_test_model_03.process_and_compare_faces(image)
+    imgshow()
+    # Return the result from the ML model
+    #return render_template('in2.html')
+    return render_template('index.html')
 
-            return jsonify({'success': True, 'message': 'Image uploaded successfully'})
-        except NoCredentialsError:
-            return jsonify({'success': False, 'message': 'AWS credentials not available'})
-
-# Start the Flask application only if this script is executed (not imported)
+    
 if __name__ == '__main__':
     app.run(debug=True)
 
